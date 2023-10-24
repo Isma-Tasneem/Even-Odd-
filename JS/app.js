700b04d22a8d0a875995fad9f7b2b678c59d2625
Isma-Tasneem <143337993+Isma-Tasneem@users.noreply.github.com>
2023-10-24 09:39:21 -0700

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+let num = Number(prompt());
+
+function number(a, b) {
+    if (num % 2 == 0) {
+        document.write("The number id Even");
+    }
+    else{
+        document.write("The number id Odd");
+    }
+}
+
+number();
+
+
